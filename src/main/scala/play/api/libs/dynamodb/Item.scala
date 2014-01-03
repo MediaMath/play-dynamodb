b770be760969986d3f52957372a5f908c545fb55
azhukov <alekseyig@hotmail.com>
2014-01-03 13:57:34 -0600

    Can process List of DdbValue(s)

--- src/main/scala/play/api/libs/dynamodb/Item.scala
+++ src/main/scala/play/api/libs/dynamodb/Item.scala
@@ -12,15 +12,15 @@ import java.nio.ByteBuffer
 
 object Item {
   def parse(m: Map[String, AttributeValue]): DdbItem = {
-    val values = m.map { case (k, v) =>
-      k -> (v match {
+    val values = m.map { case (k, value) =>
+      k -> (value match {
         case v if v.getN != null  => DdbNumber(BigDecimal(v.getN))
         case v if v.getS != null  => DdbString(v.getS)
         case v if v.getB != null  => DdbBinary(v.getB.array())
         case v if v.getNS != null => DdbSet(v.getNS.toSet.map { s: String => DdbNumber(BigDecimal(s)) })
         case v if v.getSS != null => DdbSet(v.getSS.toSet.map { s: String => DdbString(s) })
         case v if v.getBS != null => DdbSet(v.getBS.toSet.map { b: ByteBuffer => DdbBinary(b.array()) })
-        case _ => throw new IllegalStateException("Couldn't match item: " + v)
+        case _ => throw new IllegalStateException("Couldn't match item: " + value)
       })
     }.toMap
 
